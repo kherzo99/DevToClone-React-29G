6fd9fd63c12a191d6866a538b4d172108ebd9221
Karla Herzo ⚡+ <127571238+kherzo99@users.noreply.github.com>
2024-01-15 17:16:46 -0600

    aside card created

---
+++ src/components/asideCard1.jsx
@@ -0,0 +1,252 @@
+import clsx from "clsx";
+import unicon from "/src/assets/apple.svg";
+
+export default function AsideCard1() {
+  return (
+    <>
+      <div className={clsx("bg-[#f5f5f5]", "flex", "flex-col", "w-full")}>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Home</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Reading List</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Podcasts</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Videos</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Tags</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>FAQ</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Forem Shop</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Advertise on Dev</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>About</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Contact</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Guides</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Software comparision</p>
+        </div>
+        <h1 className={clsx("py-5", "font-bold")}>Other</h1>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Code of conduct</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Privacy Policy</p>
+        </div>
+        <div
+          className={clsx(
+            "flex",
+            "gap-2",
+            "text-[#404040]",
+            "hover:bg-[#e3e0f4]",
+            "hover:text-[#391fb9]",
+            "hover:underline",
+            "hover:cursor-pointer",
+            "p-2",
+            "rounded-md"
+          )}
+        >
+          <img className={clsx("w-5")} src={unicon} alt="" />
+          <p>Terms of use</p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import NavBar from "../components/NavBar";
 import PostCard from "../components/PostCard";
+import AsideCard1 from "../components/asideCard1";
 import { Link } from "react-router-dom";
 
 export default function Home() {
@@ -10,12 +11,7 @@ export default function Home() {
       <main className={clsx("text-black", "m-5 mt-0", "border border-red-500")}>
         <div className={clsx("grid grid-cols-12")}>
           <aside className={clsx("col-span-2", "border border-red-500")}>
-            <ul>
-              <li>1 item</li>
-              <li>2 item</li>
-              <li>3 item</li>
-              <li>4 item</li>
-            </ul>
+            <AsideCard1 />
           </aside>
           <section className={clsx("col-span-7", "border border-red-500")}>
             <h1>Main section</h1>
