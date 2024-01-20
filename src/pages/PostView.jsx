ccbcc821f3e0f6f7f073bf66af23f4fd53584975
Karla Herzo ⚡+ <127571238+kherzo99@users.noreply.github.com>
2024-01-19 22:59:45 -0600

    usercard added

--- src/pages/PostView.jsx
+++ src/pages/PostView.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import clsx from "clsx";
-import RightSectionCard from "../components/RightSectionCard";
+import UserCard from "../components/UserCard";
 import PostViewButtons from "../components/postViewButtons";
 import NavBar from "../components/NavBar";
 
@@ -42,63 +42,91 @@ export default function PostView() {
               </figure>
               <div className="card-body p-1 m-2">
                 <div id="profile-name-and-pic-container" className="flex">
-                  <div className="flex m-1 w-9">
+                  <div className={clsx("flex", "p-3")}>
                     <img
                       src={post.userImg}
                       alt="avatar"
-                      className="rounded-full"
+                      className={clsx("rounded-full", "w-12", "")}
                     />
                   </div>
-                  <div className="m-1">
-                    <p className="text-sm text-[#404040]">{post.userName}</p>
-                    <p className="text-xs text-[#7b7b7b]">{post.date}</p>
+                  <div className="">
+                    <p className="text-[22px] text-[#404040]">
+                      {post.userName}
+                    </p>
+                    <p className="text-[#7b7b7b]">{post.date}</p>
                   </div>
                 </div>
 
-                <h1 className={clsx("text-3xl text-[#171717] font-bold")}>
-                  {post.title}
-                </h1>
                 <div className="flex justify-between flex-col gap-5">
                   <div className="inline-flex place-items-center gap-5">
                     <a
                       href=""
-                      className="hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
+                      className="p-2 hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
                     >
                       💖 12
                     </a>
                     <a
                       href=""
-                      className="hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
+                      className="p-2 hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
                     >
                       🦄 3
                     </a>
                     <a
                       href=""
-                      className="hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
+                      className="p-2 hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
                     >
                       🤯 2
                     </a>
                     <a
                       href=""
-                      className="hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
+                      className="p-2 hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
                     >
                       🙌 2
                     </a>
                     <a
                       href=""
-                      className="hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
+                      className="p-2 hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
                     >
                       🔥 2
                     </a>
                   </div>
-                  <p>{post.postContent}</p>
+                  <h1 className={clsx("text-[36px] text-[#171717] font-bold")}>
+                    {post.title}
+                  </h1>
+                  <div className={clsx("pt-2 flex", "gap-3")}>
+                    <a
+                      href=""
+                      className="p-2 hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
+                    >
+                      #{post.hashtag1}
+                    </a>
+                    <a
+                      href=""
+                      className="p-2 hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
+                    >
+                      #{post.hashtag2}
+                    </a>
+                    <a
+                      href=""
+                      className="p-2 hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
+                    >
+                      #{post.hashtag3}
+                    </a>
+                    <a
+                      href=""
+                      className="p-2 hover:border hover:rounded-md hover:text-[#171717] hover:bg-[#f3f3f3] hover:border-[#d3d3d3] p-1 text-sm text-[#7b7b7b]"
+                    >
+                      #{post.hashtag4}
+                    </a>
+                  </div>
+                  <p className={clsx("text-[20px]")}>{post.postContent}</p>
                 </div>
               </div>
             </div>
           </section>
 
           <section className={clsx("col-span-3")}>
-            <RightSectionCard />
+            <UserCard />
           </section>
         </div>
       </main>
