49567f5c1f949bc16a6303902cf2c98b346ecbc9
Karla Herzo ⚡+ <127571238+kherzo99@users.noreply.github.com>
2024-01-22 21:28:24 -0600

    details on homer page added

--- src/components/asideCard1.jsx
+++ src/components/asideCard1.jsx
@@ -13,6 +13,12 @@ import thinkingIcon from "/src/assets/icons8-pensador-48.png";
 import conductIcon from "/src/assets/icons8-pulgar-48.png";
 import privacyIcon from "/src/assets/icons8-cara-de-nerd-48.png";
 import eyesIcon from "/src/assets/icons8-ojos-emoji-48.png";
+import twitterIcon from "/src/assets/twitter.svg";
+import gitHubIcon from "/src/assets/github.svg";
+import fbIcon from "/src/assets/facebook.svg";
+import twitchIcon from "/src/assets/twitch.svg";
+import igIcon from "/src/assets/instagram.svg";
+import mIcon from "/src/assets/circulo-m.svg";
 
 export default function AsideCard1() {
   return (
@@ -251,6 +257,166 @@ export default function AsideCard1() {
         >
           <img className={clsx("w-5")} src={eyesIcon} alt="" />
           <p>Terms of use</p>
+        </div>
+        <div
+          className={clsx(
+            "mt-5",
+            "gap-2",
+            "flex",
+            "w-max-24",
+            "justify-center"
+          )}
+        >
+          <img
+            className={clsx(
+              "w-8",
+              "opacity-80",
+              "hover:bg-[#391fb9]/20",
+              "hover:cursor-pointer",
+              "rounded-md",
+              "p-1"
+            )}
+            src={twitterIcon}
+            alt=""
+          />
+          <img
+            className={clsx(
+              "w-8",
+              "opacity-80",
+              "hover:bg-[#391fb9]/20",
+              "hover:cursor-pointer",
+              "rounded-md",
+              "p-1"
+            )}
+            src={fbIcon}
+            alt=""
+          />
+          <img
+            className={clsx(
+              "w-8",
+              "opacity-80",
+              "hover:bg-[#391fb9]/20",
+              "hover:cursor-pointer",
+              "rounded-md",
+              "p-1"
+            )}
+            src={gitHubIcon}
+            alt=""
+          />
+          <img
+            className={clsx(
+              "w-8",
+              "opacity-80",
+              "hover:bg-[#391fb9]/20",
+              "hover:cursor-pointer",
+              "rounded-md",
+              "p-1"
+            )}
+            src={igIcon}
+            alt=""
+          />
+          <img
+            className={clsx(
+              "w-8",
+              "opacity-80",
+              "hover:bg-[#391fb9]/20",
+              "hover:cursor-pointer",
+              "rounded-md",
+              "p-1"
+            )}
+            src={twitchIcon}
+            alt=""
+          />
+          <img
+            className={clsx(
+              "w-8",
+              "opacity-80",
+              "hover:bg-[#391fb9]/20",
+              "hover:cursor-pointer",
+              "rounded-md",
+              "p-1"
+            )}
+            src={mIcon}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div
+        className={clsx(
+          "mt-10",
+          " hidden",
+          "w-full",
+          "md:flex",
+          "flex-col",
+          " h-72",
+          " overflow-auto",
+          " p-2 , bg-[#ffffff] ",
+          "m-3",
+          "border ",
+          "border-[#eaeaea] ",
+          "text-[#404040]",
+          "rounded-md"
+        )}
+      >
+        <div className="card-actions justify-between flex p-1">
+          <p className="text-sm">DEV Community</p>
+          <a href="">
+            <img
+              className="w-5 p-1"
+              src="https://img.icons8.com/ios-glyphs/30/ellipsis.png"
+              alt="ellipsis"
+            />
+          </a>
+        </div>
+        <img
+          src="https://res.cloudinary.com/practicaldev/image/fetch/s--zezDNDfF--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/8nuyrw43yfedpbj2iieb.jpg"
+          alt="devtoimage"
+          className="rounded-xl p-1"
+        />
+        <h1 className="p-3 pb-0 text-2xl font-bold">
+          Its time to change it up.
+        </h1>
+        <p className="p-2 text-[#4e4e4e]">
+          You can change your feed and see more relevant posts by adding a
+          rating to different tags on DEV. Head here to adjust your weights.
+        </p>
+      </div>
+      <div className="hidden w-full md:flex flex-col card p-2  bg-[#ffffff] m-3 border border-[#eaeaea] text-[#404040] rounded-md">
+        <div className="card-actions justify-between p-1 flex">
+          <p className="text-sm">DEV Community</p>
+          <a href="">
+            <img
+              className="w-5 p-1"
+              src="https://img.icons8.com/ios-glyphs/30/ellipsis.png"
+              alt="ellipsis"
+            />
+          </a>
+        </div>
+        <div className="p-1 pt-4">
+          <p className=" text-[#4e4e4e]">
+            DEV runs on 100% open source code known as
+          </p>
+          <a href="" className="text-[#4825e8] underline ">
+            Forem.
+          </a>
+        </div>
+        <div className="p-1 ">
+          <p className=" text-[#4e4e4e]">
+            Contribute to the codebase or host your own!
+          </p>
+        </div>
+        <div className="p-1 pt-3">
+          <p className=" text-[#4e4e4e] font-bold">Check these out! 👇</p>
+        </div>
+        <div className="p-1 pb-3">
+          <a href="" className="text-[#4825e8] underline ">
+            Main Forem Repo
+          </a>
+          <br />
+          <a href="" className="text-[#4825e8] underline ">
+            Self-Host Instructions
+          </a>
         </div>
       </div>
     </>
